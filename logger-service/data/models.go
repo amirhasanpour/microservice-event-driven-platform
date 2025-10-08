5c2001abee877557489c924cf408ff0e21c79484
Amir Hasanpour <amirhasanpour.hp10@gmail.com>
2025-10-08 18:21:48 +0330

    Setup the logger data models

---
+++ logger-service/data/models.go
@@ -0,0 +1,83 @@
+package data
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		logEntry: logEntry{},
+	}
+}
+
+type Models struct {
+	logEntry logEntry
+}
+
+type logEntry struct {
+	ID 		  string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name 	  string    `bson:"name" json:"name"`
+	Data 	  string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *logEntry) Insert (entry logEntry) error {
+	collection := client.Database("logs").Collection("logs")
+
+	_, err := collection.InsertOne(context.TODO(), logEntry{
+		Name: entry.Name,
+		Data: entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+	if err != nil {
+		log.Println("Error inserting into logs: ", err)
+		return err
+	}
+
+	return nil
+}
+
+func (l *logEntry) All() ([]*logEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15 * time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	opts := options.Find()
+	opts.SetSort(bson.D{{"created_at", -1}})
+
+	cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Println("Finding all docs error: ", err)
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var logs []*logEntry
+
+	for cursor.Next(ctx) {
+		var item logEntry
+
+		err := cursor.Decode(&item)
+		if err != nil {
+			log.Println("Error decoding log into slice: ", err)
+			return nil, err
+		} else {
+			logs = append(logs, &item)
+		}
+	}
+
+	return logs, nil
+}

d1e3f99b92e594650d0e94a280e61a10671593b5
Amir Hasanpour <amirhasanpour.hp10@gmail.com>
2025-10-12 21:49:15 +0330

    Listen for RPC calls in the logger service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"logger/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -48,9 +50,12 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// register the RPC server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
 	log.Println("Starting service on port: ", webPort)
-	// go app.serve()
 	app.serve()
 }
 
@@ -63,6 +68,23 @@ func (app *Config) serve() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
